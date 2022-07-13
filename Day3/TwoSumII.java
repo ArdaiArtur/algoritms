9a44ce437b8bc143e6d80c0ae86186dfdb7c8f26
Artur Ardai <krkerdo@gmail.com>
2022-07-12 12:55:54 -1200

    yes

---
+++ Day3/TwoSumII.java
@@ -0,0 +1,17 @@
+package Day3;
+
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int i=0,j=numbers.length-1;
+        while(i<j){
+            if(numbers[i]+numbers[j]==target){
+                return new int[] {i+1,j+1};
+            } else if(numbers[i]+numbers[j]<target){
+                i++;  
+            } else{
+                j--;  
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
