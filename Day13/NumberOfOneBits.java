3415d254069266f11d000f1ca88f9ca42ae4d596
Artur Ardai <krkerdo@gmail.com>
2022-09-26 19:00:41 +0300

    ye

---
+++ Day13/NumberOfOneBits.java
@@ -0,0 +1,17 @@
+package Day13;
+
+public class NumberOfOneBits {
+    public int hammingWeight(int n) {
+        String s = Integer.toBinaryString(n);
+         int count = 0;
+         
+         for(int i = 0; i < s.length(); i++){
+             if(s.charAt(i) == '1'){
+                 count ++;
+             }
+         }
+         
+         return count;
+ 
+     }
+}
